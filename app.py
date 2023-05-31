1949eb0760e77fbde1e73eae7514cf61b0a2e5e6
TheJPMZ <monzon.jpmz@gmail.com>
2023-05-31 06:19:59 -0600

    Done

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+import memes
+
 app = Flask(__name__)
+
+current_user = None
 
 
 # Página de inicio
@@ -8,11 +12,13 @@ app = Flask(__name__)
 def home():
     # Obtener datos de las películas desde el backend
     peliculas = obtener_peliculas()
+    series = memes.get_series()
+    cortos = memes.get_cortos()
 
     # Obtener datos de las recomendaciones desde el backend
     recomendaciones = [...]  # Lista de recomendaciones obtenidas del backend
 
-    return render_template('index.html', peliculas=peliculas, recomendaciones=recomendaciones)
+    return render_template('index.html', peliculas=peliculas, recomendaciones=recomendaciones, series=series, cortos=cortos)
 
 
 # Página de preferencias
@@ -38,6 +44,10 @@ def recomendaciones():
     peliculas_recomendadas = obtener_peliculas_recomendadas()
     return render_template('recomendaciones.html', peliculas=peliculas_recomendadas)
 
+@app.route('/create_film')
+def create_film():
+    return render_template('createfilm.html', series=memes.get_series(), movies=memes.get_peliculas())
+
 
 # Ruta y función de vista para la página de actores
 @app.route('/actores')
@@ -52,87 +62,111 @@ def actores():
 # Ejemplo de función para obtener la lista de actores
 def obtener_actores():
     # Lógica para obtener la lista de actores desde tu base de datos o cualquier otra fuente de datos
-    actores = [
-        {
-            'nombre': 'Actor 1',
-            'nacionalidad': 'Estadounidense',
-            'edad': 30,
-            'genero': 'Masculino',
-            'nacimiento': '01/01/1990'
-        },
-        {
-            'nombre': 'Actor 2',
-            'nacionalidad': 'Británico',
-            'edad': 35,
-            'genero': 'Masculino',
-            'nacimiento': '15/05/1985'
-        },
-        {
-            'nombre': 'Actor 3',
-            'nacionalidad': 'Francesa',
-            'edad': 28,
-            'genero': 'Femenino',
-            'nacimiento': '10/12/1992'
-        }
-    ]
+    actores = memes.get_actores()
 
     return actores
 
 
 def obtener_peliculas():
     # Lógica para obtener la lista de películas desde tu base de datos o cualquier otra fuente de datos
-    peliculas = [
-        {
-            'titulo': 'Película 1',
-            'genero': 'Drama',
-            'duracion': '120 minutos',
-            'calificacion': 7.5,
-            'año': 2022
-        },
-        {
-            'titulo': 'Película 2',
-            'genero': 'Acción',
-            'duracion': '110 minutos',
-            'calificacion': 8.2,
-            'año': 2021
-        },
-        {
-            'titulo': 'Película 3',
-            'genero': 'Comedia',
-            'duracion': '95 minutos',
-            'calificacion': 6.9,
-            'año': 2020
-        }
-    ]
+    peliculas = memes.get_peliculas()
+    
     return peliculas
 
 
 def obtener_peliculas_recomendadas():
     # Lógica para obtener la lista de películas recomendadas desde tu base de datos o cualquier otra fuente de datos
-    peliculas_recomendadas = [
-        {
-            'titulo': 'Película 1',
-            'genero': 'Drama',
-            'duracion': '120 minutos',
-            'calificacion': 7.5,
-            'año': 2022
-        },
-        {
-            'titulo': 'Película 2',
-            'genero': 'Acción',
-            'duracion': '110 minutos',
-            'calificacion': 8.2,
-            'año': 2021
-        },
-        {
-            'titulo': 'Película 3',
-            'genero': 'Comedia',
-            'duracion': '95 minutos',
-            'calificacion': 6.9,
-            'año': 2020
-        }
-    ]
+    peliculas_recomendadas = memes.get_peliculas()
     return peliculas_recomendadas
+
+
+
+
+
+
+@app.route('/like', methods=['POST'])
+def like_movie():
+    titulo = request.form.get('titulo')
+    
+    memes.like(current_user["Email"], titulo, request.form.get('rating'))
+    
+    print(titulo, "recibió like por", current_user["Name"])
+    print("Película que recibió like:", titulo)
+    return "¡Like registrado con éxito!"
+
+@app.route('/login', methods=['POST'])
+def process_text():
+    global current_user
+    text = request.form.get('text')
+    print("Received text:", text)
+    
+    mem = memes.get_login_user(text)
+    if mem:
+        current_user = mem
+        return "Logged in! Welcome, " + mem["Name"] + "!"
+    else:
+        return "User not found! Try Again."
+    
+@app.route('/film_creation', methods=['POST'])
+def film_creation():
+    name = request.form.get('name')
+    release_year = request.form.get('release_year')
+    genre = request.form.get('genre')
+    film_type = request.form.get('film_type')
+    
+    memes.create_film(name, release_year, genre, film_type)
+    
+    return "Film created! " + name + " " + release_year + " " + genre + " " + film_type
+
+
+@app.route('/film_edition', methods=['POST'])
+def film_edition():
+    name = request.form.get('name')
+    seasons = request.form.get('seasons')
+    episodes = request.form.get('episodes')
+    avgvotes = request.form.get('avgvote')
+    
+    memes.edit_film(name, seasons, episodes, avgvotes)
+    
+    return "Film edited! " + name + " " + seasons + " " + episodes + " " + avgvotes
+
+@app.route('/film_deletion', methods=['POST'])
+def film_deletion():
+    name = request.form.get('name')
+    
+    memes.delete_movie(name)
+    
+    return "Film deleted! " + name
+
+@app.route('/node_prop_del', methods=['POST'])
+def node_prop_del():
+    name = request.form.get('name')
+    property = request.form.get('prop')
+    
+    memes.delete_property(name, property)
+    
+    return "Property deleted! " + name + " " + property
+
+
+@app.route('/unrate', methods=['POST'])
+def unrate():
+    global current_user
+    titulo = request.form.get('titulo')
+    print("Received text:", titulo)
+    
+    memes.unrate(current_user["Email"], titulo)
+    
+    return "Deleted the rating for " + titulo + " by " + current_user["Name"] + "!"
+
+@app.route('/rate', methods=['POST'])
+def rate():
+    global current_user
+    titulo = request.form.get('titulo')
+    print("Received text:", titulo)
+    
+    memes.rate(current_user["Email"], titulo, request.form.get('rating'))
+    
+    return "Rated " + titulo + " by " + current_user["Name"] + "!"
 
 
 if __name__ == '__main__':
